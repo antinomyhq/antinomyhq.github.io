2a28a394948403243796c0105b88809af26ed2c1
RushikeshGandhmal <rushikeshgandhmal@gmail.com>
2025-09-30 17:40:00 +0530

    increase z index

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -3,7 +3,7 @@ import OriginalNavbar from "@theme-original/Navbar"
 
 export default function Navbar(props: any): JSX.Element {
   return (
-    <div className="sticky top-0 z-50">
+    <div className="sticky top-0 z-[100]">
       <OriginalNavbar {...props} />
     </div>
   )
