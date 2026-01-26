3d9ca796dadd45de1a152ac20f0125fc7ff25be5
Tushar Mathur <tusharmath@gmail.com>
2026-01-26 16:48:19 +0530

    chore: prettier fixes

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -14,7 +14,8 @@ const Banner: React.FC = () => {
       <Section className="flex flex-col sm:items-center sm:text-center w-full !pb-0">
         <div className="h-full 2xl:min-h-0">
           <Heading as="h1" className="hero-banner-title">
-            AI-Native <br />Engineering
+            AI-Native <br />
+            Engineering
           </Heading>
         </div>
       </Section>
