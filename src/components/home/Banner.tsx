1564a54cd7068db81985cff4c3cb14aa09f5799f
Tushar Mathur <tushar@Tushars-MacBook-Pro.local>
2026-02-11 18:08:49 +0530

    refactor(home): extract hero animation from banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -8,12 +8,10 @@ import {pageLinks} from "@site/src/constants/routes"
 import Section from "../shared/Section"
 import SectionTitle from "../shared/SectionTitle"
 import {Star, Github} from "lucide-react"
-import HeroAnimation from "./HeroAnimation"
 
 const Banner: React.FC = () => {
   return (
-    <main className="relative">
-      <HeroAnimation />
+    <main>
       <Section className="flex flex-col items-start text-left w-full !pb-0">
         <div className="h-full 2xl:min-h-0">
           <SectionTitle title="Mission" />
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -6,12 +6,16 @@ import IntroductionGif from "./IntroductionGif"
 import WhyChooseForge from "./WhyChooseForge"
 import FinalCTA from "./FinalCTA"
 import Stats from "./Stats"
+import HeroAnimation from "./HeroAnimation"
 
 const HomePage = (): JSX.Element => {
   return (
     <div>
-      <Banner />
-      <Stats />
+      <div className="relative">
+        <HeroAnimation />
+        <Banner />
+        <Stats />
+      </div>
       <Benefits />
       <FinalCTA />
     </div>
