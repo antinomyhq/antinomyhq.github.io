faff30a151cc0aacd1b394e3c4742969febc811b
Tushar <tusharmath@gmail.com>
2026-02-11 19:42:19 +0530

    style(home): remove hero animation canvas opacity

--- src/components/home/HeroAnimation.tsx
+++ src/components/home/HeroAnimation.tsx
@@ -332,9 +332,7 @@ const HeroAnimation: React.FC = () => {
     }
   }, [])
 
-  return (
-    <canvas ref={canvasRef} className="absolute inset-0 w-full h-full pointer-events-none" style={{opacity: 0.5}} />
-  )
+  return <canvas ref={canvasRef} className="absolute inset-0 w-full h-full pointer-events-none" />
 }
 
 export default HeroAnimation
