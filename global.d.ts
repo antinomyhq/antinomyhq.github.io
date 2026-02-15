7ec4de969b3e264d19fdcc776afb24f935773fd1
Tushar <tusharmath@gmail.com>
2026-02-15 22:21:02 +0530

    fix(types): add global JSX namespace for @types/react@19 compatibility

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,21 @@
 export {}
 
+// Re-export React.JSX as global JSX namespace for @types/react@19 compatibility
+declare global {
+  /** @deprecated Use `React.JSX` instead of the global `JSX` namespace. */
+  namespace JSX {
+    type ElementType = React.JSX.ElementType
+    interface Element extends React.JSX.Element {}
+    interface ElementClass extends React.JSX.ElementClass {}
+    interface ElementAttributesProperty extends React.JSX.ElementAttributesProperty {}
+    interface ElementChildrenAttribute extends React.JSX.ElementChildrenAttribute {}
+    type LibraryManagedAttributes<C, P> = React.JSX.LibraryManagedAttributes<C, P>
+    interface IntrinsicAttributes extends React.JSX.IntrinsicAttributes {}
+    interface IntrinsicClassAttributes<T> extends React.JSX.IntrinsicClassAttributes<T> {}
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+  }
+}
+
 declare module "asciinema-player" {
   export function create(
     src: string,
